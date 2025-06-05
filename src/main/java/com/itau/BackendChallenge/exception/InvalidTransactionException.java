89b92ab762246c7643de97149be5394562fa9517
João Marcos Coutinho Nunes <jmcoutinhonunes@gmail.com>
2025-06-04 22:42:18 -0300

    feat: add transaction exception

---
+++ src/main/java/com/itau/BackendChallenge/exception/InvalidTransactionException.java
@@ -0,0 +1,17 @@
+package com.itau.BackendChallenge.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class InvalidTransactionException {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Void> invalidTransactionException(MethodArgumentNotValidException e) {
+        System.out.println(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+
+    }
+}
